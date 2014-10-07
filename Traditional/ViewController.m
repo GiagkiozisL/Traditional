fcb84b6d9cc5fbd49f54092eb31d4e7b2dc1c0f3
GiagkiozisL <giagkiozis.l@hotmail.com>
2014-10-07 10:11:41 +0300

    Initial Commit

---
+++ Traditional/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Traditional
+//
+//  Created by Giagkiozis Louloudis on 10/7/14.
+//  Copyright (c) 2014 flowerapps. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Traditional/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Traditional
+//
+//  Created by Giagkiozis Louloudis on 10/7/14.
+//  Copyright (c) 2014 flowerapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
